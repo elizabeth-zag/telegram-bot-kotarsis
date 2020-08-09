09b29b7b8ca9b0c008225f5344b2d6925fe903ab
Elizabeth <elizabeth_zag@mail.ru>
2020-08-09 22:49:42 +1000

    Fixed url in server link

--- script.py
+++ script.py
@@ -23,20 +23,29 @@ def sad_cat(message):
 
 @server.route('/' + token, methods=['POST'])
 def getMessage():
-   bot.process_new_updates(
-       [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
-   return "!", 200
+    bot.process_new_updates(
+        [telebot.types.Update.de_json(request.stream.read().decode("utf-8"))])
+    return "!", 200
 
 
 @server.route("/")
 def webhook():
-   bot.remove_webhook()
-   bot.set_webhook(url='<HEROKU Web URL>' + token)
-   return "!", 200
+    bot.remove_webhook()
+    bot.set_webhook(
+        url='https://fathomless-citadel-10711.herokuapp.com/' + token)
+    return "!", 200
+
+
+# @server.route('/', methods=["GET"])
+# def index():
+#     bot.remove_webhook()
+#     bot.set_webhook(
+#         url="https://fathomless-citadel-10711.herokuapp.com/".format('KOTarsis', token))
+#     return "Hello from Heroku!", 200
 
 
 if __name__ == "__main__":
-   server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
+    server.run(host="0.0.0.0", port=int(os.environ.get('PORT', 5000)))
 
 
 # bot.polling()
